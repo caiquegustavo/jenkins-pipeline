211d70879251827f4c0c5c39635de585c97a6c22
caiquegustavo <caiquegustavo_10@hotmail.com>
2021-01-12 14:44:37 -0300

    Commit projeto Book-Store pipeline jenkins

---
+++ MyBookStore/src/br/com/bookstore/StoreMatriz.java
@@ -0,0 +1,20 @@
+package br.com.bookstore;
+
+public class StoreMatriz {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public StoreMatriz(String name) {
+		super();
+		this.name = name;
+	}
+
+}
